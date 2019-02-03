e8a36d28779354b91abef06a93406eed841bb0b8
Milosz Lewandowski <milosz.lewandowski@gmail.com>
2019-02-03 18:41:14 +0100

    Configure Gradle Plugins Portal publisher

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -2,12 +2,15 @@ plugins {
     `kotlin-dsl`
     `maven-publish`
     id("org.jetbrains.kotlin.plugin.allopen") version "1.3.20"
+    id("com.gradle.plugin-publish") version "0.10.1"
 }
 
 gradlePlugin {
     plugins {
         register("canidropjetifier") {
             id = "com.github.plnice.canidropjetifier"
+            displayName = "Can I drop Jetifier?"
+            description = "Checks whether there are any dependencies using support library instead of AndroidX artifacts."
             implementationClass = "com.github.plnice.canidropjetifier.CanIDropJetifierPlugin"
         }
     }
@@ -28,4 +31,10 @@ publishing {
     repositories {
         maven(url = "build/repository")
     }
+}
+
+pluginBundle {
+    website = "https://github.com/plnice/can-i-drop-jetifier"
+    vcsUrl = "https://github.com/plnice/can-i-drop-jetifier"
+    tags = listOf("android", "jetifier")
 }
